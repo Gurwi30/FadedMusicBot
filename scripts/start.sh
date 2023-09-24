95a11f53140848486ce2ea608b2dfe6156943305
Gurwi30 <52247449+Gurwi30@users.noreply.github.com>
2023-09-24 13:17:12 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-screen -list | grep "FadedMusicBot" && echo "[Error] This bot is already online" || screen -S FadedMusicBot java -Dnogui=true -jar FadedMusicBot-1.0.1.jar
+screen -list | grep "FadedMusicBot" && echo "[Error] This bot is already online" || screen -S FadedMusicBot java -Dnogui=true -jar FadedMusicBot-1.0.2.jar
